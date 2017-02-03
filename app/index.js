fb6385678ccfb7e99135724fd5437e9eaa435124
Chia_ACC <chia.tsai@accumulate.com.au>
2017-02-03 16:46:07 +1100

    Props、State、Refs 與表單處理

--- app/index.js
+++ app/index.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class App extends React.Component {
+class MarkdownEditor extends React.Component {
 	constructor(props) {
 		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.rawMarkup = this.rawMarkup.bind(this);
 		this.state = {
-
-		};
+			value: 'Type some *markdown* here!',
+		}
+	}
+	handleChange() {
+		this.setState({value: this.refs.textarea.value});
+	}
+	rawMarkup() {
+		const md = new Remarkable();
+		return { __html: md.render(this.state.value) };
 	}
 	render() {
 		return (
-			<div>
-				<h1>Hello, World!t</h1>
+			<div className="MarkdownEditor">
+				<h3>Input</h3>
+				<textarea
+					onChange={this.handleChange}
+					ref="textarea"
+					defaultValue={this.state.value} />
+				<h3>Output</h3>
+				<div
+					className="content"
+					dangerouslySetInnerHTML={this.rawMarkup()}
+				/>
 			</div>
 		);
 	}
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<MarkdownEditor />, document.getElementById('app'));
